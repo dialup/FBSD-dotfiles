b0ac4f7f2f48d7fd870e96eca68b1c15a7e6a99d
dialup <marcosdialup@gmail.com>
2014-05-27 17:01:54 -0300

    OpenBox Windows Manager

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,6 +1,5 @@
 (sleep 2 && wbar) &
-(sleep 2 && tint2 -c /home/lord/.tint2rc) &
-feh --bg-scale /PATH/TO/wallpaper.jpg 
-#feh --bg-scale DD_freebsd.jpg &
+(sleep 2 && tint2 -c /PATCH/TO/.tint2rc) &
+feh --bg-scale /PATCH/TO/wallpaper.jpg 
 conky &
 conky -c /home/lord/.conkyrc2 &
--- openbox/tint2_conky_wbar/readme.txt
+++
@@ -1,3 +0,0 @@
-MOVA OS ARQUIVOS para sua pasta /home/voce/.arquivos_tintrc_wbar_conky
-
-")
